c2098906603004a547b538147ae1bb21ec4c5762
psilotec <scottadevito@gmail.com>
2016-07-11 22:59:58 -0400

    Added Styling and Content to Nav

--- client/components/nav.js
+++ client/components/nav.js
@@ -5,9 +5,17 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div>
-            nav
-        </div>
+        <nav className="navbar navbar-default navbar-fixed-top">
+            <div className="container-fluid">
+                <ul className="nav nav-pills nav-justified">
+                    <li className="navbar-text" role="presentation"><a href="#">Home</a></li>
+                    <li className="navbar-text" role="presentation"><a href="#">About Me</a></li>
+                    <li className="navbar-text" role="presentation"><a href="https://github.com/psilotec">GitHub</a></li>
+                    <li className="navbar-text" role="presentation"><a href="#">Resume</a></li>
+                    <li className="navbar-text" role="presentation"><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
