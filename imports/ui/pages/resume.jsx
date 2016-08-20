f5f4b8a35afbfda1dd4f785f3b6c0c19c4f76db3
psilotec <scottadevito@gmail.com>
2016-08-20 00:38:55 -0400

    Add Styling, Resume Component Layout and Content
    Add styling to home page/resume.  Add flexbox layout to resume component
    and add some content.

--- imports/ui/pages/resume.jsx
+++ imports/ui/pages/resume.jsx
@@ -6,11 +6,85 @@ import React from 'react';
 export const Resume = ( ) => {
     return(
         <div className="resume">
-            <ul className="nav nav-pills">
-                <li role="presentation" className="active"><a href="#">Skills</a></li>
-                <li role="presentation"><a href="#">Employment</a></li>
-                <li role="presentation"><a href="#">Education</a></li>
-            </ul>
+            <div className="filters">
+                <ul className="nav nav-pills">
+                    <li role="presentation" className="active"><a href="#">Full Resume</a></li>
+                    <li role="presentation"><a href="#">Skills</a></li>
+                    <li role="presentation"><a href="#">Professional Experience</a></li>
+                    <li role="presentation"><a href="#">Education</a></li>
+                </ul>
+            </div>
+
+            <div className="skills jumbotron">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Skills</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td>
+                                JavaScript (ES6)
+                            </td>
+                            <td>
+                                React JS
+                            </td>
+                            <td>
+                                HTML 5
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td>
+                                CSS3/SCSS/Bootstrap
+                            </td>
+                            <td>
+                                Meteor JS
+                            </td>
+                            <td>
+                                Git/GitHub
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td>
+                                NPM
+                            </td>
+                            <td>
+                                SQL/NoSQL/GraphQL
+                            </td>
+                            <td>
+                                Agile Development
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td>
+                                Testing solutions
+                            </td>
+                            <td>
+                                Java
+                            </td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="profexp jumbotron">
+                Professional Experience
+            </div>
+
+            <div className="education jumbotron">
+                Education
+            </div>
         </div>
     );
 };
