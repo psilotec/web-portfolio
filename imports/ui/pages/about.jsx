2d998fa72853f1696bc63c1ed1571b6ebf348b91
psilotec <scottadevito@gmail.com>
2016-08-28 15:02:06 -0400

    Add Font and About Content

--- imports/ui/pages/about.jsx
+++ imports/ui/pages/about.jsx
@@ -6,8 +6,8 @@ import React from 'react';
 export const About = ( ) => {
     return(
         <div className="about">
-            <h1>Welcome!</h1>
-            <p>Thanks for visiting.</p>
+            <h1>Hey, I'm Scott</h1>
+            <p>Thanks for taking the time to stop by.</p>
             <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
         </div>
     );
