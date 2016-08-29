d7bed1abbacc326ac7b57fd4502afa3fe771a382
psilotec <scottadevito@gmail.com>
2016-08-29 00:05:12 -0400

    Fix Non Active Component onscroll Issue

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -31,19 +31,26 @@ Meteor.startup( () => {
     window.onscroll = function(e) {
         var banner = document.getElementsByClassName("banner")[0],
             nav = document.getElementsByClassName("navbar")[0],
+            resume = document.getElementsByClassName("resume")[0],
             sky = document.getElementsByClassName("sky")[0],
-            range = 80,
+            range = 50,
             scrollTop = document.body.scrollTop;
 
         //sky is undefined if the About component isn't active
         if ((scrollTop > range) && (sky != undefined)) {
+            sky.classList.add("skySunny");
             banner.classList.add("blurred");
             nav.classList.add("navScroll");
-            sky.classList.add("skySunny");
         } else if ((scrollTop < range) && (sky != undefined)) {
+            sky.classList.remove("skySunny");
             banner.classList.remove("blurred");
             nav.classList.remove("navScroll");
-            sky.classList.remove("skySunny");
+        } else if ((scrollTop > range) && (resume != undefined)) {
+            banner.classList.add("blurred");
+            nav.classList.add("navScroll");
+        } else if ((scrollTop < range) && (resume != undefined)) {
+            banner.classList.remove("blurred");
+            nav.classList.remove("navScroll");
         }
     }
 );
