5ce7baccca4b1760e8d5ed4ee9a3bf3e6bebd7fa
psilotec <scottadevito@gmail.com>
2017-02-15 20:23:05 -0500

    Fix contact button layout and styling

--- imports/ui/pages/Contact.js
+++ imports/ui/pages/Contact.js
@@ -7,21 +7,15 @@ export const Contact = ( ) => {
     return(
         <div className="contact">
             <div className="contact-container">
-
-                <p>
-                    <a href="mailto:scottadevito@gmail.com">
-                        <div className="btn3"><h5>Email</h5></div>
-                    </a>
-                </p>
+                <a href="mailto:scottadevito@gmail.com">
+                    <div className="contact-btn"><h5>Email</h5></div>
+                </a>
                 
                 <img className="email-logo" src="https://s3.amazonaws.com/sdevito-web-portfolio/mailIcon1.png" />
 
-                <p>
-                    <a href="https://www.linkedin.com/pub/scott-devito/91/239/34" target="_blank">
-                        <div className="btn3"><h5>LinkedIn</h5></div>
-                    </a>
-                </p>
-
+                <a href="https://www.linkedin.com/pub/scott-devito/91/239/34" target="_blank">
+                    <div className="contact-btn"><h5>LinkedIn</h5></div>
+                </a>
             </div>
         </div>
     );
